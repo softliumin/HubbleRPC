041169fd07c6db04bd80d79701424ae2c194f638
sharper <softliumin@gmail.com>
2016-09-06 17:43:20 +0800

    获取bean成功了··

--- core/src/main/java/cc/sharper/HubbleRegistryParser.java
+++ core/src/main/java/cc/sharper/HubbleRegistryParser.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 /**
  * 解析注册中心
  * Created by liumin3 on 2016/9/2.
@@ -27,7 +30,7 @@ public class HubbleRegistryParser  implements BeanDefinitionParser
 //        int codecType=Integer.parseInt(element.getAttribute("codecType"));
 //        int timeout=Integer.parseInt(element.getAttribute("timeout"));
 
-        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+
 //        beanDefinition.setBeanClass(HubbleRegistry.class);
 //        beanDefinition.setLazyInit(false);
 //
@@ -38,6 +41,50 @@ public class HubbleRegistryParser  implements BeanDefinitionParser
 //        beanDefinition.getPropertyValues().addPropertyValue("timeout", timeout);
 //
 //        parserContext.getRegistry().registerBeanDefinition(id, beanDefinition);
+        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setLazyInit(false);
+        beanDefinition.setBeanClass(HubbleRegistry.class);
+
+        //设置属性
+//        Method[] methods = HubbleRegistry.class.getMethods();
+
+        beanDefinition.getPropertyValues().addPropertyValue("protocol",protocol);
+        beanDefinition.getPropertyValues().addPropertyValue("address",address);
+        beanDefinition.getPropertyValues().addPropertyValue("id",id);
+
+        parserContext.getRegistry().registerBeanDefinition(id, beanDefinition);
+
         return beanDefinition;
     }
+
+
+    //按照对象对象的属性来赋值
+
+    public boolean isProperty(Method method,Class beanClass)
+    {
+        String methodName = method.getName();
+        boolean flag = methodName.length()>3 && methodName.startsWith("set") && Modifier.isPublic(method.getModifiers()) && method.getParameterTypes().length==1;
+        Method getter = null;
+        if(flag)
+        {
+            Class type = method.getParameterTypes()[0];
+            try
+            {
+                //TODO
+                getter = beanClass.getMethod("get" + methodName.substring(3), new Class[0]);
+            }catch (NoSuchMethodException e)
+            {
+                try
+                {
+                    getter = beanClass.getMethod("is" + methodName.substring(3), new Class[0]);
+                }catch (NoSuchMethodException e2)
+                {
+                    ;
+                }
+            }
+        }
+        return  flag;
+    }
+
+
 }
--- show/src/test/java/TestConsumer.java
+++ show/src/test/java/TestConsumer.java
@@ -1,24 +1,43 @@
+import cc.sharper.bean.HubbleRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by liumin3 on 2016/8/30.
  */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration("/spring.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring.xml")
 public class TestConsumer
 {
-//    private Logger log = LoggerFactory.getLogger(getClass());
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public  void test()
+    {
+
+
+        HubbleRegistry registry2 =(HubbleRegistry) applicationContext.getBean("hubbleRegistry");
 //
-//    @Test
-//    public  void test()
-//    {
+        HubbleRegistry registry = null;
+
+
 //
-//    }
+//        String address = registry.getAddress();
+//
+//
+//        System.out.println(address);
+    }
 
 
 
