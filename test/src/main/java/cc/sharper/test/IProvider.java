5fc1005727b6c3a4fc74189a7bf8331b1d39c1d4
sharper <softliumin@gmail.com>
2016-09-07 14:53:48 +0800

    增加以后测试的代码层

---
+++ test/src/main/java/cc/sharper/test/IProvider.java
@@ -0,0 +1,11 @@
+package cc.sharper.test;
+
+/**
+ * 测试的provider的接口
+ * Created by liumin3 on 2016/9/7.
+ */
+public interface IProvider
+{
+    public String testMethod(String ss);
+
+}
---
+++ test2/src/main/java/cc/sharper/test/ProviderImpl.java
@@ -0,0 +1,15 @@
+package cc.sharper.test;
+
+/**
+ * 测试的provider的实现类
+ * Created by liumin3 on 2016/9/7.
+ */
+public class ProviderImpl implements  IProvider
+{
+    public String testMethod(String ss)
+    {
+        System.out.println(ss);
+
+        return "Test";
+    }
+}
