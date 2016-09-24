191e4cc8fa475da64be7a525a2311b566ee757e6
sharper <softliumin@gmail.com>
2016-09-24 20:04:21 +0800

    电脑总是重启··回家！

---
+++ core/src/main/java/cc/sharper/codec/HubbleLengthFieldBasedFrameDecoder.java
@@ -0,0 +1,116 @@
+package cc.sharper.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.nio.ByteOrder;
+import java.util.List;
+
+/**
+ *  Hubble实现的自己的FieldBasedFrameDecoder  解决半包读写
+ * Created by liumin3 on 2016/9/24.
+ *
+ * @see  io.netty.handler.codec.FixedLengthFrameDecoder
+ */
+public class HubbleLengthFieldBasedFrameDecoder extends ByteToMessageDecoder
+{
+
+
+    private final ByteOrder byteOrder;
+    private final int maxFrameLength;
+    private final int lengthFieldOffset;
+    private final int lengthFieldLength;
+    private final int lengthFieldEndOffset;
+    private final int lengthAdjustment;
+    private final int initialBytesToStrip;
+    private final boolean failFast;
+    private boolean discardingTooLongFrame;
+    private long tooLongFrameLength;
+    private long bytesToDiscard;
+
+    public HubbleLengthFieldBasedFrameDecoder(
+            int maxFrameLength,
+            int lengthFieldOffset, int lengthFieldLength) {
+        this(maxFrameLength, lengthFieldOffset, lengthFieldLength, 0, 0);
+    }
+
+    public HubbleLengthFieldBasedFrameDecoder(
+            int maxFrameLength,
+            int lengthFieldOffset, int lengthFieldLength,
+            int lengthAdjustment, int initialBytesToStrip) {
+        this(
+                maxFrameLength,
+                lengthFieldOffset, lengthFieldLength, lengthAdjustment,
+                initialBytesToStrip, true);
+    }
+
+    public HubbleLengthFieldBasedFrameDecoder(
+            int maxFrameLength, int lengthFieldOffset, int lengthFieldLength,
+            int lengthAdjustment, int initialBytesToStrip, boolean failFast) {
+        this(
+                ByteOrder.BIG_ENDIAN, maxFrameLength, lengthFieldOffset, lengthFieldLength,
+                lengthAdjustment, initialBytesToStrip, failFast);
+    }
+
+    public HubbleLengthFieldBasedFrameDecoder(
+            ByteOrder byteOrder, int maxFrameLength, int lengthFieldOffset, int lengthFieldLength,
+            int lengthAdjustment, int initialBytesToStrip, boolean failFast) {
+        if (byteOrder == null) {
+            throw new NullPointerException("byteOrder");
+        }
+
+        if (maxFrameLength <= 0) {
+            throw new IllegalArgumentException(
+                    "maxFrameLength must be a positive integer: " +
+                            maxFrameLength
+            );
+        }
+
+        if (lengthFieldOffset < 0) {
+            throw new IllegalArgumentException(
+                    "lengthFieldOffset must be a non-negative integer: " +
+                            lengthFieldOffset
+            );
+        }
+
+        if (initialBytesToStrip < 0) {
+            throw new IllegalArgumentException(
+                    "initialBytesToStrip must be a non-negative integer: " +
+                            initialBytesToStrip
+            );
+        }
+
+        if (lengthFieldOffset > maxFrameLength - lengthFieldLength) {
+            throw new IllegalArgumentException(
+                    "maxFrameLength (" + maxFrameLength + ") " +
+                            "must be equal to or greater than " +
+                            "lengthFieldOffset (" + lengthFieldOffset + ") + " +
+                            "lengthFieldLength (" + lengthFieldLength + ")."
+            );
+        }
+
+        this.byteOrder = byteOrder;
+        this.maxFrameLength = maxFrameLength;
+        this.lengthFieldOffset = lengthFieldOffset;
+        this.lengthFieldLength = lengthFieldLength;
+        this.lengthAdjustment = lengthAdjustment;
+        lengthFieldEndOffset = lengthFieldOffset + lengthFieldLength;
+        this.initialBytesToStrip = initialBytesToStrip;
+        this.failFast = failFast;
+    }
+
+
+    /**
+     * 解码
+     * @param ctx
+     * @param in
+     * @param out
+     * @throws Exception
+     */
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
+    {
+
+    }
+}
---
+++ core/src/main/java/cc/sharper/error/HubbleErrorException.java
@@ -0,0 +1,10 @@
+package cc.sharper.error;
+
+/**
+ * Hubble 统一的异常类
+ * Created by liumin3 on 2016/9/24.
+ */
+public class HubbleErrorException extends RuntimeException
+{
+    
+}
