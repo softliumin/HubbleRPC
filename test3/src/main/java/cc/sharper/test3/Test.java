da464ef07302111b2f1d980c6f333de1b0f85cdf
wangye35 <wangye11@jd.com>
2016-10-20 14:06:16 +0800

    格式化第一个测试类，修正第二个测试类的路径

--- test3/src/main/java/cc/sharper/test3/Test.java
+++ test3/src/main/java/cc/sharper/test3/Test.java
@@ -8,16 +8,14 @@ import java.util.Random;
  * 带权重的的随机轮训
  * Created by liumin3 on 2016/9/8.
  */
-public class Test
-{
+public class Test {
     /**
-     *
      * 这个实现太牛逼了，简单但是真的有效！
      * 测试随机数 带权重的随机数
+     *
      * @param args
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Xigua x1 = new Xigua(200);
         Xigua x2 = new Xigua(300);
         Xigua x3 = new Xigua(500);
@@ -29,51 +27,44 @@ public class Test
         list.add(x2);
         list.add(x3);
         Random random = new Random();
-        for (int x =0;x<100000000;x++)
-        {
+        for (int x = 0; x < 100000000; x++) {
             int rand = random.nextInt(1000);
-            int sum= 0;
-            for(int i=0;i<list.size();i++)
-            {
+            int sum = 0;
+            for (int i = 0; i < list.size(); i++) {
                 sum += list.get(i).getWeight();//武将的出现机率
-                if(rand<=sum)
-                {
-                    int ss =  list.get(i).getWeight();
-                    if(ss==200)
-                    {
-                        s1+=1;
+                if (rand <= sum) {
+                    int ss = list.get(i).getWeight();
+                    if (ss == 200) {
+                        s1 += 1;
                     }
-                    if(ss==300)
-                    {
-                        s2+=1;
+                    if (ss == 300) {
+                        s2 += 1;
                     }
-                    if(ss==500)
-                    {
-                        s3+=1;
+                    if (ss == 500) {
+                        s3 += 1;
                     }
                     break;
                 }
             }
         }
-        System.out.println("s1:"+s1);
-        System.out.println("s2:"+s2);
-        System.out.println("s3:"+s3);
+        System.out.println("s1:" + s1);
+        System.out.println("s2:" + s2);
+        System.out.println("s3:" + s3);
     }
 }
 
-class Xigua
-{
+class Xigua {
     private int weight;
-    public  Xigua(int weight)
-    {
+
+    public Xigua(int weight) {
         this.weight = weight;
     }
-    public int getWeight()
-    {
+
+    public int getWeight() {
         return weight;
     }
-    public void setWeight(int weight)
-    {
+
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 }
